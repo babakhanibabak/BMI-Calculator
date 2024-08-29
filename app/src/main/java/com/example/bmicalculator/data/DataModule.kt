5bbd555f1ebfd8ddd9383054439abe9a4938746f
Babak <babakhanibabak@gmail.com>
2024-08-29 21:26:32 +0330

    create DataModule

---
+++ app/src/main/java/com/example/bmicalculator/data/DataModule.kt
@@ -0,0 +1,17 @@
+package com.example.bmicalculator.data
+
+import com.example.bmicalculator.data.repository.BmiRepositoryImpl
+import com.example.bmicalculator.domain.repository.BmiRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataModule {
+    @Binds
+    @Singleton
+    fun bindBmiRepository(impl:BmiRepositoryImpl):BmiRepository
+}
