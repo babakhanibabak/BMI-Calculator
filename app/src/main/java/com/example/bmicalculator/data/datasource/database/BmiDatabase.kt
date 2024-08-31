7cf0c251fbcabdb2b6443d1ee5e65f73f8df0c12
Babak <babakhanibabak@gmail.com>
2024-08-31 21:53:28 +0330

    Add BmIDatabase

---
+++ app/src/main/java/com/example/bmicalculator/data/datasource/database/BmiDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.bmicalculator.data.datasource.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.bmicalculator.data.datasource.database.dao.BmiDao
+import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
+
+@Database(entities = [BmiEntity::class], version = 1)
+abstract class BmiDatabase:RoomDatabase() {
+abstract fun bmiDao():BmiDao
+}
