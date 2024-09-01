7330368fa16556d39b3b19a915bafee34827ea7b
Babak <babakhanibabak@gmail.com>
2024-09-01 23:20:26 +0330

    Edit BmiRepository

--- app/src/main/java/com/example/bmicalculator/domain/repository/BmiRepository.kt
+++ app/src/main/java/com/example/bmicalculator/domain/repository/BmiRepository.kt
@@ -1,9 +1,10 @@
 package com.example.bmicalculator.domain.repository
 
 import com.example.bmicalculator.domain.model.BmiModel
+import com.example.bmicalculator.domain.model.BmiModelResult
 
 interface BmiRepository {
     suspend fun getBmiModel(): BmiModel
-    suspend fun saveBmiModel(bmiModel: BmiModel)
+    suspend fun saveBmiModel(bmiModelResult: BmiModelResult)
 
 }
