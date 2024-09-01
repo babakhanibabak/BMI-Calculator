12cc80ddea3fb3dca6c83e91ad58ef108880516d
Babak <babakhanibabak@gmail.com>
2024-09-01 23:18:51 +0330

    Edit BmiEntity

--- app/src/main/java/com/example/bmicalculator/data/datasource/database/entity/BmiEntity.kt
+++ app/src/main/java/com/example/bmicalculator/data/datasource/database/entity/BmiEntity.kt
@@ -2,6 +2,8 @@ package com.example.bmicalculator.data.datasource.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.bmicalculator.domain.model.BmiModel
+import com.example.bmicalculator.domain.model.BmiModelResult
 
 @Entity("bmi_data")
 data class BmiEntity(
@@ -12,7 +14,21 @@ data class BmiEntity(
     val weight:Int
 )
 
+
+@Entity("bmi_result")
 data class BmiCalculationResult(
     val bmi:Float,
     val bmiCategory:String
+)
+
+fun BmiEntity.toDomainModel()=BmiModel(
+    age=age,
+    height=height,
+    weight=weight
+
+)
+
+fun BmiCalculationResult.toDomainModel()=BmiModelResult(
+    bmi=bmi,
+    bmiCategory=bmiCategory
 )
