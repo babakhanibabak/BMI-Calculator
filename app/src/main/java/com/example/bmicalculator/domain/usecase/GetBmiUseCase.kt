1a1113189cf9bdd6bc930d41ac70f0db252f1d1b
Babak <babakhanibabak@gmail.com>
2024-09-02 10:59:04 +0330

    Create GetBmiUseCase

---
+++ app/src/main/java/com/example/bmicalculator/domain/usecase/GetBmiUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.bmicalculator.domain.usecase
+
+import com.example.bmicalculator.domain.model.BmiModel
+import com.example.bmicalculator.domain.model.BmiModelResult
+import com.example.bmicalculator.domain.repository.BmiRepository
+import javax.inject.Inject
+
+class GetBmiUseCase @Inject constructor(
+    private val bmiRepository: BmiRepository
+) {
+    suspend fun execute(): BmiModel {
+        return bmiRepository.getBmiModel()
+
+    }
+
+}
