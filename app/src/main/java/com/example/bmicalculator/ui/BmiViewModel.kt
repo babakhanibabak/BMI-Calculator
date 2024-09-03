a9944a1fa89ecf70bf6f256fcb2885bfe6a77981
Babak <babakhanibabak@gmail.com>
2024-09-03 19:56:13 +0330

    Create BmiViewModel

---
+++ app/src/main/java/com/example/bmicalculator/ui/BmiViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.bmicalculator.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bmicalculator.data.datasource.database.dao.BmiDao
+import com.example.bmicalculator.data.datasource.database.entity.BmiCalculationResult
+import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class BmiViewModel @Inject constructor(
+    private val bmiDao: BmiDao
+) : ViewModel() {
+
+    val bmiEntity: BmiEntity = bmiDao.getAllBmi()
+    fun calculateBmi(age: Int, height: Int, weight: Int): BmiCalculationResult {
+        val heightInMeters = height.toFloat() / 100
+        val bmi = weight.toFloat() / (heightInMeters * heightInMeters)
+        val bmiCategory = when {
+            bmi < 18.5 -> "Underweight"
+            bmi in 18.5..24.9 -> "Normal weight"
+            bmi in 25.0..29.9 -> "Overweight"
+            bmi in 30.0..34.9 -> " obese class I"
+            bmi in 35.0..39.9 -> "obese class II"
+            else -> "obese class III"
+        }
+        return BmiCalculationResult(bmi, bmiCategory)
+    }
+
+    suspend fun insertBmiData(bmiEntity: BmiEntity) {
+        bmiDao.insertBmiData(bmiEntity)
+    }
+
+    fun deleteAllBmiData() {
+        viewModelScope.launch {
+            bmiDao.deleteBmi()
+        }
+    }
+
+}
