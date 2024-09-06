2eeab566db22a451361d33df6e3e88690c558e62
Babak <babakhanibabak@gmail.com>
2024-09-06 23:39:58 +0330

    edit BmiRepositoryImpl

--- app/src/main/java/com/example/bmicalculator/data/repository/BmiRepositoryImpl.kt
+++ app/src/main/java/com/example/bmicalculator/data/repository/BmiRepositoryImpl.kt
@@ -1,39 +1,22 @@
 package com.example.bmicalculator.data.repository
 
 import com.example.bmicalculator.data.datasource.database.dao.BmiDao
-import com.example.bmicalculator.data.datasource.database.entity.BmiCalculationResult
 import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
-import com.example.bmicalculator.data.datasource.database.entity.toDomainModel
-import com.example.bmicalculator.domain.model.BmiModel
-import com.example.bmicalculator.domain.model.BmiModelResult
 import com.example.bmicalculator.domain.repository.BmiRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class BmiRepositoryImpl @Inject constructor(
     private val bmiDao: BmiDao
 ) : BmiRepository {
-override suspend fun getBmiModel():BmiModel{
-    return bmiDao.getAllBmi().toDomainModel()}
 
-    suspend fun insertBmiModel(bmiModel: BmiModel) {
-        val entity=BmiEntity(
-            age = bmiModel.age,
-            height = bmiModel.height,
-            weight = bmiModel.weight
-        )
-        bmiDao.insertBmiData(
-            entity
-        )
+    override suspend fun insert(record: BmiEntity) {
+        bmiDao.insertBmiData(record)
     }
 
-
-
-    override suspend fun saveBmiModel(bmiModelResult: BmiModelResult) {
-        val entityResult=BmiCalculationResult(
-            bmiCategory = bmiModelResult.bmiCategory,
-            bmi = bmiModelResult.bmi
-        )
-
-
+    override suspend fun getAllRecords(): Flow<List<BmiEntity>> = flow{
+emit(bmiDao.getAllBmi())
     }
 }
+
