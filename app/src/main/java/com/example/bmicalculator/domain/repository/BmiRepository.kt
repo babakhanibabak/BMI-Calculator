7191342d9bf47a7ae22e8a36713ec43054cf940b
Babak <babakhanibabak@gmail.com>
2024-09-06 23:39:36 +0330

    edit BmiRepository

--- app/src/main/java/com/example/bmicalculator/domain/repository/BmiRepository.kt
+++ app/src/main/java/com/example/bmicalculator/domain/repository/BmiRepository.kt
@@ -1,10 +1,12 @@
 package com.example.bmicalculator.domain.repository
 
+import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
 import com.example.bmicalculator.domain.model.BmiModel
 import com.example.bmicalculator.domain.model.BmiModelResult
+import kotlinx.coroutines.flow.Flow
 
 interface BmiRepository {
-    suspend fun getBmiModel(): BmiModel
-    suspend fun saveBmiModel(bmiModelResult: BmiModelResult)
+    suspend fun insert( record:BmiEntity)
+    suspend fun getAllRecords():Flow<List<BmiEntity>>
 
 }
