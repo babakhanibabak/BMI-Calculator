a37882a7ded8924ae1ebb89964b38bc626b0a0dc
Babak <babakhanibabak@gmail.com>
2024-09-07 20:33:11 +0330

    Create State

---
+++ app/src/main/java/com/example/bmicalculator/ui/BmiUi/BmiScreenState.kt
@@ -0,0 +1,12 @@
+package com.example.bmicalculator.ui.BmiUi
+
+import androidx.core.app.GrammaticalInflectionManagerCompat.GrammaticalGender
+
+data class BmiScreenState(
+
+    val bmi:String,
+    val idealWeight: String,
+    val fat: String,
+
+
+    )
