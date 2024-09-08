72b6b0605bfaac0739fcd0d6f34e09c0b92ed589
Babak <babakhanibabak@gmail.com>
2024-09-08 18:45:16 +0330

    Edit BmiScreen

--- app/src/main/java/com/example/bmicalculator/domain/usecase/GetBmiUseCase.kt
+++ app/src/main/java/com/example/bmicalculator/domain/usecase/GetBmiUseCase.kt
@@ -5,12 +5,12 @@ import com.example.bmicalculator.domain.model.BmiModelResult
 import com.example.bmicalculator.domain.repository.BmiRepository
 import javax.inject.Inject
 
-class GetBmiUseCase @Inject constructor(
-    private val bmiRepository: BmiRepository
-) {
-    suspend fun execute(): BmiModel {
-        return bmiRepository.getBmiModel()
-
-    }
-
-}
+//class GetBmiUseCase @Inject constructor(
+//    private val bmiRepository: BmiRepository
+//) {
+//    suspend fun execute(): BmiModel {
+//        return bmiRepository.getBmiModel()
+//
+//    }
+//
+//}
