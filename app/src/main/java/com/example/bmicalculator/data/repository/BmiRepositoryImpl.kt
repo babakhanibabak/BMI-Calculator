727e515466fcc142425151ab896a2b68846d3a74
Babak <babakhanibabak@gmail.com>
2024-09-09 19:01:12 +0330

    Edit BmiRepositoryImpl

--- app/src/main/java/com/example/bmicalculator/data/repository/BmiRepositoryImpl.kt
+++ app/src/main/java/com/example/bmicalculator/data/repository/BmiRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.bmicalculator.data.repository
 
 import com.example.bmicalculator.data.datasource.database.dao.BmiDao
 import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
+import com.example.bmicalculator.domain.model.BmiModel
 import com.example.bmicalculator.domain.repository.BmiRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -15,8 +16,19 @@ class BmiRepositoryImpl @Inject constructor(
         bmiDao.insertBmiData(record)
     }
 
-    override suspend fun getAllRecords(): Flow<List<BmiEntity>> = flow{
-emit(bmiDao.getAllBmi())
+    override suspend fun getAllRecords(): Flow<List<BmiEntity>> = flow {
+        emit(bmiDao.getAllBmi())
+    }
+    private fun BmiEntity.toDomainModel(): BmiModel {
+        return BmiModel(
+            age = this.age,
+            height = this.height,
+            weight = this.weight,
+            gender = this.gender,
+            bmi = this.bmi,
+            idealWeight = this.idealWeight,
+            bodyFat = this.bodyFat
+        )
     }
 }
 
