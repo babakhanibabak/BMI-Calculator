5a5cfa152533d81448c176dc8472f7f105fb5fac
Babak <babakhanibabak@gmail.com>
2024-09-15 23:22:59 +0330

    Edit BmiViewModel

--- app/src/main/java/com/example/bmicalculator/ui/bmi/BmiViewModel.kt
+++ app/src/main/java/com/example/bmicalculator/ui/bmi/BmiViewModel.kt
@@ -2,24 +2,18 @@ package com.example.bmicalculator.ui.bmi
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
 import com.example.bmicalculator.domain.model.Gender
-import com.example.bmicalculator.domain.repository.BmiRepository
 import com.example.bmicalculator.domain.usecase.GetBmiUseCase
 import com.example.bmicalculator.domain.usecase.GetBodyFatUseCase
 import com.example.bmicalculator.domain.usecase.GetIdealWeightUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BmiViewModel @Inject constructor(
-    private val repository: BmiRepository,
     private val getBmiUseCase: GetBmiUseCase,
     private val getIdealWeightUseCase: GetIdealWeightUseCase,
     private val getBodyFatUseCase: GetBodyFatUseCase,
@@ -75,41 +69,4 @@ class BmiViewModel @Inject constructor(
 
     // TODO: Move this logic to HistoryScreen ================================
 
-    private val _bmiRecords = MutableStateFlow<List<BmiEntity>>(emptyList())
-    val bmiRecords: StateFlow<List<BmiEntity>> = _bmiRecords.asStateFlow()
-
-    fun calculateBmi(record: BmiEntity) {
-        viewModelScope.launch {
-            try {
-                val heightInMeters = record.height / 100
-                val bmi = record.weight / (heightInMeters * heightInMeters)
-                val bmiCategory = when {
-                    bmi < 18.5 -> "Underweight"
-                    bmi in 18.5..24.9 -> "Normal weight"
-                    bmi in 25.0..29.9 -> "Overweight"
-                    bmi in 30.0..34.9 -> " obese class I"
-                    bmi in 35.0..39.9 -> "obese class II"
-                    else -> "obese class III"
-                }
-                insertBmiData(record)
-            } finally {
-            }
-
-        }
-    }
-
-    fun insertBmiData(record: BmiEntity) {
-        viewModelScope.launch {
-            repository.insert(record)
-            loadRecords()
-        }
-    }
-
-    private fun loadRecords() {
-        viewModelScope.launch {
-            repository.getAllRecords().collect { records ->
-                _bmiRecords.value = records
-            }
-        }
-    }
 }
