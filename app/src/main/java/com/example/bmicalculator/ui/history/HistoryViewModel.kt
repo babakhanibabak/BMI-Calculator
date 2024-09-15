a61911965552de6b425122a93061ec42a34142ff
Babak <babakhanibabak@gmail.com>
2024-09-15 23:24:03 +0330

    Create HistoryViewModel

---
+++ app/src/main/java/com/example/bmicalculator/ui/history/HistoryViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.bmicalculator.ui.history
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
+import com.example.bmicalculator.domain.repository.BmiRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class HistoryViewModel @Inject constructor(
+    private val repository: BmiRepository
+) :ViewModel(){
+
+    private val _bmiRecords = MutableStateFlow<List<BmiEntity>>(emptyList())
+    val bmiRecords: StateFlow<List<BmiEntity>> = _bmiRecords.asStateFlow()
+
+    fun calculateBmi(record: BmiEntity) {
+        viewModelScope.launch {
+            try {
+                val heightInMeters = record.height / 100
+                val bmi = record.weight / (heightInMeters * heightInMeters)
+                val bmiCategory = when {
+                    bmi < 18.5 -> "Underweight"
+                    bmi in 18.5..24.9 -> "Normal weight"
+                    bmi in 25.0..29.9 -> "Overweight"
+                    bmi in 30.0..34.9 -> " obese class I"
+                    bmi in 35.0..39.9 -> "obese class II"
+                    else -> "obese class III"
+                }
+                insertBmiData(record)
+            } finally {
+            }
+
+        }
+    }
+
+    fun insertBmiData(record: BmiEntity) {
+        viewModelScope.launch {
+            repository.insert(record)
+            loadRecords()
+        }
+    }
+
+    private fun loadRecords() {
+        viewModelScope.launch {
+            repository.getAllRecords().collect { records ->
+                _bmiRecords.value = records
+            }
+        }
+    }
+}
