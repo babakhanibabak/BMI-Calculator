be1eb9e8e836b21e3b9956bcf6f90f6fb1989d2b
Babak <babakhanibabak@gmail.com>
2024-09-26 21:02:27 +0330

    Add more Colors inside theme

--- app/src/main/java/com/example/bmicalculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/bmicalculator/ui/theme/Color.kt
@@ -6,9 +6,16 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFFFA500)
+val LightOrange = Color(0xFFFFD700)
+
+val LightBlue = Color(0xFF42DDFF)
+val LightGreen = Color(0xFF4DC885)
 
 val underweightColor = Color(0xFF42DDFF)  // Custom color for Underweight
 val normalColor = Color(0xFF4CC885)       // Custom color for Normal weight
