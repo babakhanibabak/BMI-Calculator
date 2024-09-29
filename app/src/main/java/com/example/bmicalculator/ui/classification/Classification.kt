f6ce331bc01a23855b9fb08c3e56a2d9adc9d1bb
Babak <babakhanibabak@gmail.com>
2024-09-29 23:48:26 +0330

    Create ClassificationScreen

---
+++ app/src/main/java/com/example/bmicalculator/ui/classification/Classification.kt
@@ -0,0 +1,53 @@
+package com.example.bmicalculator.ui.classification
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.wear.compose.material.Text
+import com.example.bmicalculator.ui.bmi.BmiScreenState
+import com.example.bmicalculator.ui.bmi.BmiViewModel
+import com.example.bmicalculator.ui.theme.BMICalculatorTheme
+
+@Composable
+fun ClassificationScreen(
+    viewModel: BmiViewModel = hiltViewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
+
+   ClassificationScreenContent(uiState=uiState)
+}
+
+@Composable
+fun ClassificationScreenContent(uiState: BmiScreenState,) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "BMI Classification",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,)
+    }
+}
+
+@Preview
+@Composable
+private fun ClassificationScreenContentPreview() {
+    BMICalculatorTheme {
+        ClassificationScreenContent(uiState = BmiScreenState())
+    }
+}
