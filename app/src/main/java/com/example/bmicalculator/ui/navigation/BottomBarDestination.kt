00c32a25df15a0f89dc88251b92a556d18bd4b86
Babak <babakhanibabak@gmail.com>
2024-09-30 22:24:45 +0330

    Edit BottomBarDestination

--- app/src/main/java/com/example/bmicalculator/ui/navigation/BottomBarDestination.kt
+++ app/src/main/java/com/example/bmicalculator/ui/navigation/BottomBarDestination.kt
@@ -2,7 +2,9 @@ package com.example.bmicalculator.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.automirrored.outlined.List
+import androidx.compose.material.icons.automirrored.outlined.Send
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -10,6 +12,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 sealed class BottomBarDestination(val route: String) {
     data object Map : BottomBarDestination("bmi_screen")
     data object Points : BottomBarDestination("history_screen")
+    data object classification: BottomBarDestination("classification_screen")
 }
 
 data class BottomNavigationItem(
@@ -31,5 +34,11 @@ data class BottomNavigationItem(
             text = "History",
             route = BottomBarDestination.Points.route
         ),
+        BottomNavigationItem(
+            selectedIcon = Icons.AutoMirrored.Filled.Send,
+            unSelectedIcon = Icons.AutoMirrored.Outlined.Send,
+            text = "Classification",
+            route = BottomBarDestination.classification.route
+        )
     )
 }
