17078e95a2b1bd1b13dcb5abceb13e9502d731a1
Babak <babakhanibabak@gmail.com>
2024-10-29 22:13:50 +0330

    Edit ClassificationScreen

--- app/src/main/java/com/example/bmicalculator/ui/classification/ClassificationScreen.kt
+++ app/src/main/java/com/example/bmicalculator/ui/classification/ClassificationScreen.kt
@@ -49,21 +49,7 @@ fun ClassificationScreenContent(
             textAlign = TextAlign.Center,
             fontSize = 24.sp,)
 
-        uiState.bmiClassifications?.let { classifications ->
-            classifications.forEach { classification ->
-               Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            classification.backgroundColor,
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .padding(all = 8.dp),
-                    text = classification.title,
-                    fontWeight = FontWeight.Bold,
-                    color = classification.color,
-                )
-            }
+        uiState.bmiClassifications?.let {
         }
     }
 }
