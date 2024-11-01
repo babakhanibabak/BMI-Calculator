ca7db2320cbeea0ee4e240fef3daf37e39a2fac7
Babak <babakhanibabak@gmail.com>
2024-11-01 19:02:15 +0330

    Edit Daos

--- app/src/main/java/com/example/bmicalculator/data/datasource/database/di/DaosModule.kt
+++ app/src/main/java/com/example/bmicalculator/data/datasource/database/di/DaosModule.kt
@@ -1,6 +1,7 @@
 package com.example.bmicalculator.data.datasource.database.di
 
 import com.example.bmicalculator.data.datasource.database.BmiDatabase
+import com.example.bmicalculator.data.datasource.database.dao.AccountDao
 import com.example.bmicalculator.data.datasource.database.dao.BmiDao
 import dagger.Module
 import dagger.Provides
@@ -16,4 +17,11 @@ object DaosModule {
     fun provideLocationDao(db: BmiDatabase) : BmiDao {
         return db.bmiDao()
     }
+
+    @Provides
+    @Singleton
+    fun provideAccountDao(db: BmiDatabase): AccountDao {
+        return db.accountDao()
+    }
+
 }
