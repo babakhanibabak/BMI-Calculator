5662faa29ad7621819eef6b59dd9717a5b817209
Babak <babakhanibabak@gmail.com>
2024-11-01 19:01:32 +0330

    Edit BmiDatabase

--- app/src/main/java/com/example/bmicalculator/data/datasource/database/BmiDatabase.kt
+++ app/src/main/java/com/example/bmicalculator/data/datasource/database/BmiDatabase.kt
@@ -2,15 +2,18 @@ package com.example.bmicalculator.data.datasource.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.bmicalculator.data.datasource.database.dao.AccountDao
 import com.example.bmicalculator.data.datasource.database.dao.BmiDao
 import com.example.bmicalculator.data.datasource.database.entity.BmiEntity
+import com.example.bmicalculator.data.datasource.database.entity.UserEntity
 
 @Database(
-    entities = [BmiEntity::class],
+    entities = [BmiEntity::class, UserEntity::class],
     version = 1,
     exportSchema = true,
     autoMigrations = [],
 )
 abstract class BmiDatabase : RoomDatabase() {
     abstract fun bmiDao(): BmiDao
+    abstract fun accountDao():AccountDao
 }
