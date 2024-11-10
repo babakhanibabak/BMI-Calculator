121b3cfae1a436416bc5d341f173647845f98cba
Babak <babakhanibabak@gmail.com>
2024-11-10 17:27:49 +0330

    Add BmiClassificationCircle to BmiScreen

--- app/src/main/java/com/example/bmicalculator/ui/bmi/BmiScreen.kt
+++ app/src/main/java/com/example/bmicalculator/ui/bmi/BmiScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -238,18 +239,27 @@ fun BmiScreenContent(
 
             uiState.bmiClassifications?.let { classifications ->
                 classifications.forEach { classification ->
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(
-                                classification.backgroundColor,
-                                shape = RoundedCornerShape(8.dp)
-                            )
-                            .padding(all = 8.dp),
-                        text = classification.title,
-                        fontWeight = FontWeight.Bold,
-                        color = classification.color,
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    ) {
+                        BmiClassificationCircle(circleColor = classification.BmiCircleColor)
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(
+                                    classification.backgroundColor,
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                                .padding(all = 8.dp),
+                            text = classification.title,
+                            fontWeight = FontWeight.Bold,
+                            color = classification.color,
+                        )
+                    }
                 }
             }
         }
